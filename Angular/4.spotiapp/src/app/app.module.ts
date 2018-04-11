c209c967541212816512fb1fdbfbf61262b6350e
DESKTOP-0E7SMAF\Marlotina <inaki.arrieta@gmail.com>
2018-04-11 20:53:17 +0200

    spotiapp

---
+++ Angular/4.spotiapp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AppRoutingModule } from './app.routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
---
+++ Angular/4.spotiapp/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '', pathMatch:'full', redirectTo:'home' }
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes, {useHash:true});
