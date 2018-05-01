08efa44073b16595926f172a044a86687661763d
DESKTOP-0E7SMAF\Marlotina <inaki.arrieta@gmail.com>
2018-05-01 20:05:54 +0200

    style

--- Angular/6.miscelania/src/app/components/ng-style/ng-style.component.ts
+++ Angular/6.miscelania/src/app/components/ng-style/ng-style.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
-    <p style="font-size:10px;">
+    <p [style.fontSize.px]="size">
       Hello word, it is a label
     </p>
+    <button  class="btn btn-primary" (click)="size = size + 5">
+      <i class="fa fa-plus"></i>
+    </button>
+    <button class="btn btn-primary" (click)="size = size - 5">
+      <i class="fa fa-minus"></i>
+    </button>
+
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
 
+  size:number= 10;
   constructor() { }
 
   ngOnInit() {
